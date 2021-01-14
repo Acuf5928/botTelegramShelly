050c5545cc2e43e3265bd2d0f44e1090d895b8d4
Acuf5928 <alberto.cuffaro@hotmail.it>
2021-01-14 15:53:04 +0100

    create code_list_device

--- code_list_device.py
+++ code_list_device.py
@@ -1,7 +1,21 @@
 from telegram import Update
 from telegram.ext import CallbackContext
 
+from code_helper import retrieveDevicesInfo
+
 
 def listDevice(update: Update, context: CallbackContext):
     chat_id = update.message.chat_id
-    context.bot.send_message(chat_id=chat_id, text="Funzione non implementata")
+
+    try:
+        listDevice = retrieveDevicesInfo()
+    except Exception:
+        context.bot.send_message(chat_id=chat_id, text="Errore nel db")
+        return
+
+    message = "Lista device:\n"
+
+    for element in listDevice:
+        message = message + element.name + "\n"
+
+    context.bot.send_message(chat_id=chat_id, text=message)
